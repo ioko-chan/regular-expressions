4582ba5c5086a38f3cb4b8eae773ed6db0473816
Ekaterina <solomennicova555@gmail.com>
2022-11-02 16:29:44 +0200

    edit task 1

--- src/index.php
+++ src/index.php
@@ -8,5 +8,5 @@ $str = 'a1b2c3';
 $func = function ($number){
     return pow($number[0],3);
 };
-$result =  preg_replace_callback('#(\d+)#',$func,$str);
+$result =  preg_replace_callback('/[0-9]+/',$func,$str);
 echo $result;
